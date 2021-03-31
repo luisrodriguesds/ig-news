bb55541f3e0d6eb2630080a05edd4fd3312a74a5
Luis <luisitaloar@gmail.com>
2021-03-30 21:05:55 -0300

    feat: Estilização com sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>Olá <span>Mundo</span></h1>
+    </div>
+  )
+}
