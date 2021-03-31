f4c69edf7aa7fef6365330d4a7845dcb01948ff6
Luis <luisitaloar@gmail.com>
2021-03-30 22:34:47 -0300

    feat: pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
+
+import styles from "./index.module.scss"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Olá <span>Mundo</span></h1>
-    </div>
+    <>
+      <Head>
+        <title> Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90/ month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl"/>
+      </main>
+    </>
   )
 }
