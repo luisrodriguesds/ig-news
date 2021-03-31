f18dbc58fddfff0546e16199c61d31047655234a
Luis <luisitaloar@gmail.com>
2021-03-31 18:42:02 -0300

    feat: Verificado para salvar no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,7 +13,43 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
 
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: {
+                  email: user.email
+                }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch (error) {
+        console.log(error);
+        return false
+      }
+    },
+  }
   // A database is optional, but required to persist accounts in a database
   // database: process.env.DATABASE_URL,
 })
